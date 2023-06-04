0b70435ed5a40cd29749d004f8df729a5c6132bd
Akmal <akmalbekortiqovv@gmail.com>
2023-06-04 16:44:56 +0500

    the last commit js

--- main.js
+++ main.js
@@ -20,18 +20,3 @@ function raqamniTekshir() {
       input.value = modifiedValue;
    }
 }
-
-
-// Fulll
-// function raqamniTekshir() {
-//    var input = document.getElementById("raqam");
-//    if (input.value.length > 0 && parseInt(input.value) < 998) {
-//       input.value = "+998" + input.value;
-//    }
-//    var value = input.value.trim();
-
-//    if (value.length > 0) {
-//       var modifiedValue = value.replace(/(\d{6})(\d{2})(\d{2})(\d{2})/, "$1 $2 $3 $4");
-//       input.value = modifiedValue;
-//    }
-// }
